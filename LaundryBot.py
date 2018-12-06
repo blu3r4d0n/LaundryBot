29b3017a7d445922c2132b2ac1c1c9fc312e9d5c
Philip Nelson <philipnelson99@icloud.com>
2018-12-06 11:08:34 -0500

    Some code clean up and comments

--- LaundryBot.py
+++ LaundryBot.py
@@ -9,14 +9,12 @@ def getData(room):
     data=requests.get(room)
     parsed = xmltodict.parse(data.content)
     return parsed
-def getWashers(laundryroomData):
+def getWashers(laundryroomData):  ##May merge getWashers and getDryers at some point
     availWash=0
-    for appliance in laundryroomData['laundry_room']['appliances']['appliance']:
+    for appliance in laundryroomData['laundry_room']['appliances']['appliance']: 
         if appliance['appliance_type'] == 'WASHER':
             if appliance['status'] == 'Available':
                 availWash=availWash+1
-#            if appliance['out_of_service']=='1':
-#                print("Yep")
     return availWash
 def getDryers(laundryroomData):
     availDry=0
@@ -26,23 +24,22 @@ def getDryers(laundryroomData):
                 availDry=availDry+1
             
     return availDry
-#if  availWash == 0
-
 def Phelps():
     laundryRooms = {
-        "Phelps 1st Floor":'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946215',
-        "Phelps 2nd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946216',
-        "Phelps Basement": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194625',
-        "Richardson": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194629',
-        "Roddey 1st Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194621',
-        "Roddey 2nd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194622',
-        "Roddey 3rd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194623',
-        "Thomson East": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194627',
-        "Thomson West": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194628',
-        "Wofford":'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946210',
+        "Phelps 1st Floor":'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946215', ############################
+        "Phelps 2nd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946216',############################
+        "Phelps Basement": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194625',  ############################
+        "Richardson": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194629',       # This API is publicly known
+        "Roddey 1st Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194621', # so I don't feel it's      
+        "Roddey 2nd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194622', # dangerous to put on GitHub
+        "Roddey 3rd Floor": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194623', # (It's alright :) ) 
+        "Thomson East": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194627',     ############################
+        "Thomson West": 'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=194628',     ############################
+        "Wofford":'http://api.laundryview.com/room/?api_key=8c31a4878805ea4fe690e48fddbfffe1&method=getAppliances&location=1946210',          ############################
         }
     for key, value in laundryRooms.items():
-        altText="Currently, there are " + str(getWashers(getData(value))) + " washers and "+ str(getDryers(getData(value))) + " dryers available in the "+ key +" laundry room."
+        #altText="Currently, there are " + str(getWashers(getData(value))) + " washers and "+ str(getDryers(getData(value))) + " dryers available in the "+ key +" laundry room."
         tweet= key +": "+ str(getWashers(getData(value))) +  " W / " + str(getDryers(getData(value))) + " D"
         print(tweet)
+        #tweet(tweet)
 Phelps()
